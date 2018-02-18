c59d4e4e5f306cf46800f80228170d5e46db6b07
Joseph <josembi@gmail.com>
2018-02-18 01:03:49 +0000

    Sort Registration Issue to Universal Naming

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class JosembiController < Devise::JosembiController 
+class RegistrationsController < Devise::JosembiController 
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :shots
-  devise_for :users, controllers: { josembi: 'josembi' }
+  devise_for :users, controllers: { registrations: 'registrations' }
 
 	root "shots#index"
 end
